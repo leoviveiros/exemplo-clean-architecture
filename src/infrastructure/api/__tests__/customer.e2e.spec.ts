37aa640b34242ba3341b5d27eef118277837eac4
Leonardo Pinto <leoviveiros@gmail.com>
2022-06-03 10:39:20 -0300

    list customer endpoint

--- src/infrastructure/api/__tests__/customer.e2e.spec.ts
+++ src/infrastructure/api/__tests__/customer.e2e.spec.ts
@@ -1,10 +1,10 @@
 import { app, sequelize } from '../express';
 import resquest from 'supertest';
 
-
 describe('Customer e2e test', () => {
 
     beforeEach(async () => {
+        await sequelize.dropAllSchemas({logging: false});
         await sequelize.sync();
     });
 
@@ -41,6 +41,48 @@ describe('Customer e2e test', () => {
             });
 
         expect(response.status).toBe(500);
+    });
+
+
+    it('should get all customers', async () => {
+        await resquest(app)
+            .post('/customers')
+            .send({
+                name: 'John Doe',
+                address: {
+                    street: '123 Main St',
+                    number: 123,
+                    zipCode: '12345',
+                    city: 'Anytown'
+                }
+            });
+
+        await resquest(app)
+            .post('/customers')
+            .send({
+                name: 'Jane Doe',
+                address: {
+                    street: '1234 Main St',
+                    number: 1234,
+                    zipCode: '12345',
+                    city: 'Anytown'
+                }
+            });
+
+        const response = await resquest(app)
+            .get('/customers')
+            .send();
+
+        expect(response.status).toBe(200);
+        expect(response.body.customers.length).toBe(2);
+
+        const joe = response.body.customers[0];
+        expect(joe.name).toBe('John Doe');
+        expect(joe.address.street).toBe('123 Main St');
+        
+        const jane = response.body.customers[1];
+        expect(jane.name).toBe('Jane Doe');
+        expect(jane.address.street).toBe('1234 Main St');
     });
 
 })
