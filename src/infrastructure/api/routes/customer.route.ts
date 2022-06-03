4e9d2a9a332a089feafc4aa872b9330652967daf
Leonardo Pinto <leoviveiros@gmail.com>
2022-06-03 10:39:20 -0300

    list customer endpoint

--- src/infrastructure/api/routes/customer.route.ts
+++ src/infrastructure/api/routes/customer.route.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import CreateCustomerUseCase from '@usecase/customer/create/create.customer.usecase';
+import ListCustomerUseCase from '@usecase/customer/list/list.customer.usecase';
 import CustomerRepository from '@infra/customer/repository/sequelize/customer.repository';
 
 export const customerRoute = express.Router();
@@ -25,4 +26,17 @@ customerRoute.post('/', async (req: Request, res: Response) => {
     } catch (error) {
         res.status(500).send(error);
     }
-})
+});
+
+customerRoute.get('/', async (req: Request, res: Response) => {
+    const repository = new CustomerRepository();
+    const useCase = new ListCustomerUseCase(repository);
+
+    try {
+        const output = await useCase.execute({});
+
+        res.send(output);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
