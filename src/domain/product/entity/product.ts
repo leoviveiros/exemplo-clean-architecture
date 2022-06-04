6914a700bd2f06c809616e523a3f6325d9e5d686
Leonardo Pinto <leoviveiros@gmail.com>
2022-06-04 13:22:42 -0300

    notication pattern implementation  on product

--- src/domain/product/entity/product.ts
+++ src/domain/product/entity/product.ts
@@ -1,19 +1,17 @@
+import Entity from '../../shared/entity/entity.abstract';
+import NotificationError from '../../shared/notification/notification.error';
 import ProductInterface from './product.interface';
 
-export default class Product implements ProductInterface {
-    private _id: string;
+export default class Product extends Entity implements ProductInterface {
     private _name: string;
     private _price: number;
 
     constructor(id: string, name: string, price: number) {
+        super();
         this._id = id;
         this._name = name;
         this._price = price;
         this.validate();
-    }
-
-    get id(): string {
-        return this._id;
     }
 
     get name(): string {
@@ -34,16 +32,31 @@ export default class Product implements ProductInterface {
         this.validate();
     }
 
-    validate(): boolean {
-        if (this._id.length === 0) {
-            throw new Error("Id is required");
+    validate() {
+        if (!this._id || this._id.length === 0) {
+            this.notification.addError({
+                context: 'product',
+                message: 'Id is required'
+            });
         }
-        if (this._name.length === 0) {
-            throw new Error("Name is required");
+
+        if (!this._name || this._name.length === 0) {
+            this.notification.addError({
+                context: 'product',
+                message: 'Name is required'
+            });
         }
+
         if (this._price <= 0) {
-            throw new Error("Price must be greater than zero");
+            this.notification.addError({
+                context: 'product',
+                message: 'Price must be greater than zero'
+            });
         }
-        return true;
+
+        if (this.notification.hasErrors()) {
+            throw new NotificationError(this.notification.errors);
+        }
+
     }
 }
