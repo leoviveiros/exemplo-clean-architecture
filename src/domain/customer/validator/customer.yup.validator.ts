9701300d51eefa66f30d21bc1933c1f2b6735d38
Leonardo Pinto <leoviveiros@gmail.com>
2022-06-04 15:44:23 -0300

    yup import rafactoring

--- src/domain/customer/validator/customer.yup.validator.ts
+++ src/domain/customer/validator/customer.yup.validator.ts
@@ -1,20 +1,21 @@
+import { object, string, ValidationError } from 'yup';
+
 import ValidatorInterface from '../../shared/validator/validator.interface';
 import Customer from '../entity/customer';
-import * as yup from 'yup';
 
 export default class CustomerYupValidator implements ValidatorInterface<Customer> {
     
     validate(customer: Customer): void {
         try {
-            yup.object().shape({
-                id: yup.string().required('Id is required'),
-                name: yup.string().required('Name is required')
+            object().shape({
+                id: string().required('Id is required'),
+                name: string().required('Name is required')
             }).validateSync({
                 id: customer.id,
                 name: customer.name
             }, { abortEarly: false })
         } catch (errors) {
-            const err = errors as yup.ValidationError;
+            const err = errors as ValidationError;
             
             err.errors.forEach(error => customer.addNotificationError({
                 context: 'customer',
